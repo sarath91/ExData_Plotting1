446575e78fcfad0b13b27f4a67352eb54437e49c
sarath91 <36625978+sarath91@users.noreply.github.com>
2018-02-27 02:43:16 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,52 @@
+setwd("/Users/SARATH/Desktop/R Programming")
+df <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+               nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+df1 <- subset(df, Date %in% c("1/2/2007","2/2/2007"))
+df1$Date <- as.Date(df1$Date, format = "%d/%m/%Y")
+df1$Datetime <- as.POSIXct(paste(df1$Date, df1$Time))
+
+## Plot 1
+hist(df1$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
+
+
+##Plot 2
+with(df1, {
+  plot(Global_active_power~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+})
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
+
+## Plot 3
+with(df1, {
+  plot(Sub_metering_1~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
+
+## Plot 4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(df1, {
+  plot(Global_active_power~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~Datetime, type="l", 
+       ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~Datetime, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="")
+})
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
